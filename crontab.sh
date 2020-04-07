49a6ee0fc2e45a2d3e786ebd78945b7aa710db91
Damien Erambert <damien.erambert@me.com>
2020-04-07 10:03:27 -0700

    build script

--- crontab.sh
+++ crontab.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
 cd `dirname $0`
-`which npm` run start
+# paths can be relative to the current user that owns the crontab configuration
+
+# $(which node) returns the path to the current node version
+# either the one specified as `default` alias in NVM or a specific version set above
+# executing `nvm use 4 1> /dev/null` here won't work!
+
+$(which node) src/index.js
