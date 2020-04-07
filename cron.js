a96637ee21c92360d3964f94bb9cdfa1e2e9f2b4
Damien Erambert <damien.erambert@me.com>
2020-04-07 10:27:44 -0700

    :fire:

--- cron.js
+++
@@ -1,8 +0,0 @@
-const cron = require('node-cron');
-const execa = require('execa');
-
-cron.schedule('* * * * *', async () => {
-  const { stderr, stdout } = await execa('npm', ['start']);
-  console.error(stderr);
-  console.log(new Date(), stdout);
-});
