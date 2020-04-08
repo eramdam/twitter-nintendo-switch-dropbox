4f49e1191e3370f4a39b1b8ef10c26ddc1983b2c
Damien Erambert <damien.erambert@me.com>
2020-04-08 10:38:39 -0700

    add logs to cron

--- src/cron.ts
+++ src/cron.ts
@@ -2,9 +2,11 @@ import cron from 'node-cron';
 import { grabMedia } from './grabMedia';
 
 (async () => {
+  console.log('Gonna grab tweets media', new Date());
   await grabMedia();
 
   cron.schedule('* * * * *', async () => {
+    console.log('Gonna grab tweets media', new Date());
     await grabMedia();
   });
 })();
