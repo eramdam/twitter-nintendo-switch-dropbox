281ff671e2b748d821562c3774767fbbeb117cb9
Damien Erambert <damien.erambert@me.com>
2020-04-19 14:30:27 -0700

    use express server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
 import { grabMedia } from './grabMedia';
+import { AddressInfo } from 'net';
 
-grabMedia();
+dotenv.config();
+
+const app = express();
+
+app.all('/' + process.env.SCRIPT_ENDPOINT, async (_req, res) => {
+  await grabMedia();
+
+  return res.sendStatus(200);
+});
+
+// The `PORT` variable is set on glitch.com
+const listener = app.listen(process.env.PORT || 3000, () => {
+  const info = listener.address() as AddressInfo;
+  console.log(`Running server on http://localhost:${info.port}`);
+});
